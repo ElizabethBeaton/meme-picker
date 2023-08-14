07ac4434f19be9fc197d422b1b15c1ea77c406d5
ElizabethBeaton <elizabethbeaton23@outlook.com>
2023-08-14 20:34:46 +0100

    removed duplicate using logical not operator

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-import { catsData } from "./data.js";
+import { catsData } from "/data.js";
 
 const emotionRadios = document.getElementById("emotion-radios");
 
 function getEmotionsArray(cats) {
   const emotionsArray = [];
+
   for (let cat of cats) {
     for (let emotion of cat.emotionTags) {
-      emotionsArray.push(emotion);
+      if (!emotionsArray.includes(emotion)) {
+        emotionsArray.push(emotion);
+      }
     }
   }
   return emotionsArray;
@@ -17,18 +20,17 @@ function renderEmotionsRadios(cats) {
   const emotions = getEmotionsArray(cats);
   for (let emotion of emotions) {
     radioItems += `
-    <div class="radio">
-    <label for="${emotion}">${emotion}</label>
-    <input 
-    type="radio"
-    id="${emotion}"
-    value="${emotion}"
-    name="emotion"
-    >
-    </div>
-    `;
+        <div class="radio">
+            <label for="${emotion}">${emotion}</label>
+            <input
+            type="radio"
+            id="${emotion}"
+            value="${emotion}"
+            name="emotions"
+            >
+        </div>`;
   }
-
   emotionRadios.innerHTML = radioItems;
 }
+
 renderEmotionsRadios(catsData);
