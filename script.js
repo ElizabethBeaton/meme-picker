6fcca4f198cba7a834f8bc1381f91caa7c55dc2b
ElizabethBeaton <elizabethbeaton23@outlook.com>
2023-08-23 16:52:27 +0100

    completed closeModal function

--- script.js
+++ script.js
@@ -5,10 +5,17 @@ const getImageBtn = document.getElementById("get-image-btn");
 const gifsOnlyOption = document.getElementById("gifs-only-option");
 const memeModalInner = document.getElementById("meme-modal-inner");
 const memeModal = document.getElementById("meme-modal");
+const modalCloseBtn = document.getElementById("meme-modal-close-btn");
 
 emotionRadios.addEventListener("change", highlightCheckedOption);
 
 getImageBtn.addEventListener("click", renderCat);
+
+modalCloseBtn.addEventListener("click", closeModal);
+
+function closeModal() {
+  memeModal.style.display = "none";
+}
 
 function highlightCheckedOption(e) {
   const radios = document.getElementsByClassName("radio");
@@ -50,13 +57,11 @@ function getSingleCatObject() {
 function renderCat() {
   const catObject = getSingleCatObject();
 
-  memeModalInner.innerHTML = `
-    <img 
-    class="cat-img" 
-    src="./images/${catObject.image}"
-    alt="${catObject.alt}"
-    >
-    `;
+  memeModalInner.innerHTML = `<img 
+        class="cat-img" 
+        src="./images/${catObject.image}"
+        alt="${catObject.alt}"
+        >`;
   memeModal.style.display = "flex";
 }
 
